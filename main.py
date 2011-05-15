48723f382ba3096d5e892968576b6eecb0163d33
kyl191 <github@kyl191.net>
2011-05-15 21:37:54 +0800

    Need to completely modular-ify this. Got the checksums in files down, but the directory traversal is still problematic

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-import db, hash, mp3, os, sys
-print sys.argv
+import db, hash, mp3, os, sys, re
+def hashAndAdd(file):
+	title, artist, album = mp3.getid3(file)
+	tempfile = mp3.stripid3(file)
+	hash = hash.sha512file(tempfile[1])
+	os.close(tempfile[0])
+	os.remove(tempfile[1])
+	print title, artist, album
+	print db.insertIntoDB(dbcursor, str(title), str(artist), str(album), hash, os.path.abspath(file))
+	dbconn.commit()
+
 os.chdir(sys.argv[1])
 dbPath = "/home/kyl191/mp3dedup/mp3dedup.db"
 dbconn = db.startDB(dbPath)
 dbcursor = dbconn.cursor()
-for root, dirs, files in os.walk('.'):
-	print root, dirs, files
+# I believe os.walk is a far more optimal implementation, just have to figure out how it works
+#for root in os.walk('.'):
+#	for dirs in root:
+#		for files in dirs:
+#			print files
+files = []
 for file in os.listdir("."):
 	print file
-title, artist, album = mp3.getid3(file)
-tempfile = mp3.stripid3(file)
-hash = hash.sha512file(tempfile[1])
-os.close(tempfile[0])
-os.remove(tempfile[1])
-print title, artist, album
-print db.insertIntoDB(dbcursor, str(title), str(artist), str(album), hash, os.path.abspath(file))
+	if re.search(".mp3",file):
+		files.append(file)
+		
+for file in files:
+	hashAndAdd(file)
+
 dbcursor.close()
 dbconn.commit()
