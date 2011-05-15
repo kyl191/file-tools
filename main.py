162102383420c72fd42494e094dbd1d8e755f229
kyl191 <github@kyl191.net>
2011-05-15 21:52:54 +0800

    In short: I'm going to be looking up how to get os.walk running the way I want it to.

--- main.py
+++ main.py
@@ -16,11 +16,6 @@ dbconn = db.startDB(dbPath)
 dbcursor = dbconn.cursor()
 # End initial setup
 
-# I believe os.walk is a far more optimal implementation, just have to figure out how it works
-#for root in os.walk('.'):
-#	for dirs in root:
-#		for files in dirs:
-#			print files
 
 # Do the actual hashing of the files
 for file in os.listdir("."):
@@ -28,6 +23,16 @@ for file in os.listdir("."):
 	if re.search(".mp3",file):
 		hashAndAdd(file)
 		
+# I believe os.walk is a far more optimal implementation.
+# Just have to figure out how it works.
+# Also, race conditions, what happens if I move folders around while the script is running?
+# It should only care about the folder that it's in. 
+# Though, since it would use os.listdir at the root, still has a race condition...
+# Net result, might as well use os.walk
+#for root in os.walk('.'):
+#	for dirs in root:
+#		for files in dirs:
+#			print files
 
 # Close the cursor & commit the DB one last time just for good measure
 dbcursor.close()
