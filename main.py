3ba8a69285cbda763e793d729b3e8d21dd21f920
kyl191 <github@kyl191.net>
2011-05-15 21:21:13 +0800

    dev copy of main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import db, hash, mp3, os
+import db, hash, mp3, os, sys
+print sys.argv
+os.chdir(sys.argv[1])
 dbPath = "/home/kyl191/mp3dedup/mp3dedup.db"
-db = db.startDB(dbPath)
-file = "test.mp3"
-info = mp3.getid3(file)
-hash = hash.sha512file(mp3.stripid3(file))
-print db.insertIntoDB(db, info, hash, os.path.abspath(file))
+dbconn = db.startDB(dbPath)
+dbcursor = dbconn.cursor()
+for root, dirs, files in os.walk('.'):
+	print root, dirs, files
+for file in os.listdir("."):
+	print file
+title, artist, album = mp3.getid3(file)
+tempfile = mp3.stripid3(file)
+hash = hash.sha512file(tempfile[1])
+os.close(tempfile[0])
+os.remove(tempfile[1])
+print title, artist, album
+print db.insertIntoDB(dbcursor, str(title), str(artist), str(album), hash, os.path.abspath(file))
+dbcursor.close()
+dbconn.commit()
