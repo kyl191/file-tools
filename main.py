859f63e3b2815512ec0334aceac8afd51e9c288f
kyl191 <github@kyl191.net>
2011-05-15 21:44:20 +0800

    Commenting

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ def hashAndAdd(file):
 	print db.insertIntoDB(dbcursor, str(title), str(artist), str(album), hashresult, os.path.abspath(file))
 	dbconn.commit()
 
+# Initial setup of DB & search path
 os.chdir(sys.argv[1])
 dbPath = "/home/kyl191/mp3dedup/mp3dedup.db"
 dbconn = db.startDB(dbPath)
 dbcursor = dbconn.cursor()
+# End initial setup
+
 # I believe os.walk is a far more optimal implementation, just have to figure out how it works
 #for root in os.walk('.'):
 #	for dirs in root:
 #		for files in dirs:
 #			print files
+
 files = []
 for file in os.listdir("."):
 	print file
@@ -27,5 +31,6 @@ for file in os.listdir("."):
 for file in files:
 	hashAndAdd(file)
 
+# Close the cursor & commit the DB one last time just for good measure
 dbcursor.close()
 dbconn.commit()
