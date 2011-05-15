6018a38efe12bbc2424ced75916eac601c2290cc
kyl191 <github@kyl191.net>
2011-05-15 19:50:15 +0800

    basic db code

---
+++ db.py
@@ -0,0 +1,16 @@
+import sqlite3
+def startDB(dbPath):
+	try:
+		conn = sqlite3.connect(dbPath)
+		c = conn.cursor()
+		c.execute("SELECT * from mp3dedup")
+	except sqlite3.OperationalError, e:
+		print "SQLite3 Error : Operational Error", e.args[0]
+	except sqlite3.DatabaseError, e:
+		print "SQLite3 Error : Database Error", e.args[0]
+	except sqlite3.Error, e:
+		print "SQLite 3: Unknown Error", e.args[0]
+	except:
+		print "Unknown Error"
+	return c
+
