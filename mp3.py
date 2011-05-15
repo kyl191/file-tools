2e3a82f0d763f402717d5048e5758f5ee4f45d52
kyl191 <github@kyl191.net>
2011-05-15 20:39:13 +0800

    added function to get id3 info

--- mp3.py
+++ mp3.py
@@ -11,8 +11,17 @@ def stripid3(file):
 	# Returns a *tuple* of the file reference & file path!
 	return temp
 
+def getid3(file):
+	audio = ID3(file)
+	title = audio["TIT2"]
+	artist = audio["TPE1"]
+	album = audio["TALB"]
+	return title,artist,album
+
 file = "test.mp3"
+title, artist, album = getid3(file)
 test = stripid3(file)
-print test[1]
+#print test[1]
+#print title, ":", artist, ":" , album
 os.close(test[0])
 os.remove(test[1])
