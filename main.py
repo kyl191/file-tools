a197fa011ad86f5bfd5bbd4a400a80b858ca5390
kyl191 <github@kyl191.net>
2011-05-17 10:15:20 +0800

    Got the os.walk() implementation working.
    The documentation is annoyingly obtuse, but pen + paper & experimentation worked it out! =D

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import db, hash, mp3, os, sys, re
+from os.path import join
 def hashAndAdd(file):
 	title, artist, album = mp3.getid3(file)
 	tempfile = mp3.stripid3(file)
@@ -16,23 +17,12 @@ dbconn = db.startDB(dbPath)
 dbcursor = dbconn.cursor()
 # End initial setup
 
-
-# Do the actual hashing of the files
-for file in os.listdir("."):
-	print file
-	if re.search(".mp3",file):
-		hashAndAdd(file)
-		
-# I believe os.walk is a far more optimal implementation.
-# Just have to figure out how it works.
-# Also, race conditions, what happens if I move folders around while the script is running?
-# It should only care about the folder that it's in. 
-# Though, since it would use os.listdir at the root, still has a race condition...
-# Net result, might as well use os.walk
-#for root in os.walk('.'):
-#	for dirs in root:
-#		for files in dirs:
-#			print files
+for root, dirs, files in os.walk('.'):
+	for filename in files:
+		print os.path.abspath(join(root,filename))
+		if re.search(".mp3",filename):
+			hashAndAdd(os.path.abspath(join(root,filename)))
+			print "found MP3 file: ", os.path.abspath(join(root,filename))
 
 # Close the cursor & commit the DB one last time just for good measure
 dbcursor.close()
