8398cd4ef6f22b61f7a79068b4ffff40428c2828
kyl191 <github@kyl191.net>
2011-08-26 23:17:41 +0800

    Remove similarly named files (ie. _2 appended to filename)

--- db.py
+++ db.py
@@ -11,34 +11,34 @@ def startDB(dbPath):
 		print("SQLite3 Error : Database Error", e.args[0])
 	except sqlite3.Error as e:
 		print("SQLite 3: Unknown Error", e.args[0])
-	except:
-		print("Unknown Error")
+	except Exception as e:
+		print("Unknown Error", e)
 	return conn
 
 def insertIntoDB(db, info):
 	# info = title, artist, album, hash, filepath, mtime
 	try:
-		db.execute(u"INSERT into mp3dedup VALUES (Null, ?, ?, ?, ?, ?, ?)",info)
+		db.execute(u"INSERT into filededup VALUES (Null, ?, ?, ?, ?, ?, ?)",info)
 	except sqlite3.Error as e:
 		print("SQLite 3: Unknown Error", e.args[0])
 	return info
 
 def checkIfExists(db,filepath):
 	try:
-		(results,) = db.execute(u"SELECT COUNT(*) from mp3dedup WHERE filepath = ?",[filepath])
+		(results,) = db.execute(u"SELECT COUNT(*) from filededup WHERE filepath = ?",[filepath])
 	except sqlite3.Error as e:
 		print("SQLite 3: Unknown Error", e.args[0])
 	else:
 		if results[0] > 0:
-			(mtime,) = db.execute(u"SELECT mtime from mp3dedup WHERE filepath = ?",[filepath])
+			(mtime,) = db.execute(u"SELECT mtime from filededup WHERE filepath = ?",[filepath])
 			return (results[0],mtime[0])
 		else:
 			return (0, -1)
 def updateDB(db, info):
-	# info = title, artist, album, hash, filepath, mtime
+	# info = hash, filepath, mtime
 	try:
-		db.execute(u"UPDATE mp3dedup set id3_title = :title, id3_artist = :artist, id3_album = :album, hash = :hash, mtime = :mtime where filepath = :path",
-			{"title":info.title, "artist":info.artist, "album":info.album, "hash":info.hash, "mtime":info.mtime, "path":info.filepath})
+		db.execute(u"UPDATE filededup set hash = :hash, mtime = :mtime where filepath = :path",
+			{"hash":info.hash, "mtime":info.mtime, "path":info.filepath})
 	except sqlite3.Error as e:
 		print("SQLite 3: Unknown Error", e.args[0])
 	return info
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import db, hash, mp3, os, sys, re
+import db, hash, os, sys, re
 from os.path import join
 from collections import namedtuple
-mp3info = namedtuple('mp3info', "title artist album hash filepath mtime")
+fileinfo = namedtuple('fileinfo', "hash filepath mtime")
 
 def hashAndAdd(file):
-	# Check if it's a valid MP3 file first by trying to get the ID3 info
-	try:
-		title, artist, album = mp3.getid3(file)
-	except Exception as e:
-		# So far the only exception is an invalid ID3 header found, so not much to grab
-		print(e)
-		return
 	mtime = os.path.getmtime(file)
 	(exists,dbmtime) = db.checkIfExists(dbcursor, unicode(str(os.path.abspath(file)).decode('utf-8')))
 	update = False
@@ -24,12 +17,8 @@ def hashAndAdd(file):
 			# Need to come up with an update statement...
 			print("Updating", file)
 			update = True
-	
-	tempfile = mp3.stripid3(file)
-	hashresult = hash.sha512file(tempfile[1])
-	os.close(tempfile[0])
-	os.remove(tempfile[1])
-	info = mp3info(title, artist, album, unicode(hashresult), unicode(str(os.path.abspath(file)).decode('utf-8')), mtime)
+	hashresult = hash.sha512file(file)
+	info = fileinfo(unicode(hashresult), unicode(str(os.path.abspath(file)).decode('utf-8')), mtime)
 	if not update:
 		print(info,"Ins")
 		db.insertIntoDB(dbcursor, info)
@@ -40,24 +29,46 @@ def hashAndAdd(file):
 
 # Initial setup of DB & search path
 os.chdir(sys.argv[1])
-dbPath = "/home/kyl191/mp3dedup/mp3dedup.db"
-dbconn = db.startDB(dbPath)
-dbcursor = dbconn.cursor()
+#dbPath = "filededup.db"
+#dbconn = db.startDB(dbPath)
+#dbcursor = dbconn.cursor()
+
+# If there's something wrong with the db, we should try & create it.
+#try:
+#	
+#except Exception as e:
+#	sql = """BEGIN TRANSACTION;
+#CREATE TABLE filededup (
+#    "id" INTEGER,
+#    "hash" TEXT,
+#    "filepath" TEXT,
+#    "mtime" INTEGER
+#);
+#CREATE INDEX "path_index" on "filededup" (filepath ASC);
+#CREATE INDEX "hash_index" on "filededup" (hash ASC);
+#COMMIT;"""
+
+
 # End initial setup
 
-# Walk the directory structure looking for MP3 files
+# Walk the directory structure
 for root, subfolders, files in os.walk('.'):
 	# Mention what path we're working in.
 	print("Working in", os.path.abspath(root))
 	# Since root contains the working folder, and we'll move onto subfolders later, 
 	# We only care about the filename
 	for filename in files:
-		# So, for each file, check if it has an MP3 extension
-		if re.search(".mp3",filename,re.IGNORECASE):
-			# If is does, hash & add it to the db
-			hashAndAdd(os.path.abspath(join(root,filename)))
-			#print "found MP3 file: ", os.path.abspath(join(root,filename))
+		# If is does, hash & add it to the db
+		#hashAndAdd(os.path.abspath(join(root,filename)))
+		(filenameprefix, dot, filenamesuffix) = filename.rpartition(".")
+		dup = filenameprefix + "_2." + filenamesuffix
+		if os.path.exists(dup):
+			hash1 = hash.sha512file(filename)
+			hash2 = hash.sha512file(dup)
+			if hash1 == hash2:
+				print "Removing " + dup + "!"
+				os.remove(dup)
 
 # Close the cursor & commit the DB one last time just for good measure
-dbcursor.close()
-dbconn.commit()
+#dbcursor.close()
+#dbconn.commit()
