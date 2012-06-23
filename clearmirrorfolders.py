8f32e8d28f97c440630dc1b596beab5e88f65d74
Kyle Lexmond <github@kyl191.net>
2012-06-23 15:12:18 +0800

    Implement barebones merging functionality

--- clearmirrorfolders.py
+++ clearmirrorfolders.py
@@ -1,4 +1,4 @@
-import hash, os, sys, re
+import hash, os, sys, re, shutil
 from os.path import join, getsize
 
 source_dir = os.path.abspath(sys.argv[1])
@@ -26,6 +26,15 @@ for root, subfolders, files in os.walk(source_dir):
 				space_saved = space_saved + os.path.getsize(dup)
 				print "[" + str(deleted_files) + "] Removing " + dup
 				#os.remove(dup)
+	# Merge files that are in the dup folder but aren't in the source folder
+	dup_folder_files = os.listdir(os.path.normpath(dup_folder))
+	for file in dup_folder_files:
+		src_path = os.path.abspath(join(dup_folder, file))
+		dst_path = os.path.abspath(join(root, file))
+		print src_path, dst_path
+		if not os.path.exists(dst_path):
+			#shutil.move(src_path, dst_path)
+			print "Moved " + src_path + " to " + dst_path
 	if os.path.exists(dup_folder) and not os.listdir(dup_folder):
 		os.rmdir(dup_folder)
 print("Deleted " + str(deleted_files) + ", saving " + str(space_saved) + " bytes of space")
