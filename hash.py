47d3193259f7c81c7767be19541941977d9f3906
Kyle Lexmond <github@kyl191.net>
2012-06-24 14:13:18 +0800

    Print stuff about time taken to read files.

--- hash.py
+++ hash.py
@@ -1,4 +1,4 @@
-import hashlib
+import hashlib, os, time
 
 def sha512file(file):
 	sha512 = hashlib.sha512()
@@ -6,9 +6,12 @@ def sha512file(file):
 		f = open(file,'rb')
 	except IOError:
 		print("IO Error, unable to open file", file)
+	start = time.clock()
 	while True:
 		data = f.read(10240) # SHA512 take 512 bits, 512*20=10240, read more just to be nice & efficient
 		if not data:
 			break
 		sha512.update(data)
+	end = time.clock()
+	print "Took", end - start, "seconds to read", os.stat(file).st_size, "bytes"
 	return sha512.hexdigest()
