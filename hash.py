b7fce998ca9d78f334a13e069ab3ff80ef652c7a
Kyle Lexmond <github@kyl191.net>
2012-06-24 14:38:09 +0800

    Comment the decision to read by chunks

--- hash.py
+++ hash.py
@@ -8,6 +8,8 @@ def sha512file(file):
 		print("IO Error, unable to open file", file)
 	start = time.clock()
 	while True:
+		# Read in by chunks so we don't max out RAM if we're reading a large file
+		# Difference in time between reading by chunks & reading entire file at once for small files is approx 0.03 secs
 		data = f.read(20480) # SHA512 take 512 bits, 512*40=20480, read more just to be nice & efficient
 		if not data:
 			break
