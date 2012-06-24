22b652803e525ba7cd40846dd0a398dce6c85798
Kyle Lexmond <github@kyl191.net>
2012-06-24 14:34:59 +0800

    Trivial difference between the two, so just stay with the existing form.

--- hash.py
+++ hash.py
@@ -6,20 +6,14 @@ def sha512file(file):
 		f = open(file,'rb')
 	except IOError:
 		print("IO Error, unable to open file", file)
-	start2 = time.clock()
-	hash2 = hashlib.sha512(f.read()).hexdigest()
-	end2 = time.clock()
 	start = time.clock()
-	f.seek(0)
 	while True:
-		data = f.read(10240) # SHA512 take 512 bits, 512*20=10240, read more just to be nice & efficient
+		data = f.read(20480) # SHA512 take 512 bits, 512*40=20480, read more just to be nice & efficient
 		if not data:
 			break
 		sha512.update(data)
-	hash1 = sha512.hexdigest()
+	hash = sha512.hexdigest()
 	end = time.clock()
 	print "Took", end - start, "seconds to read", os.stat(file).st_size, "bytes"
-	print "Took", end2 - start2, "seconds to read", os.stat(file).st_size, "bytes at once"
 	f.close()
-	assert(hash1 == hash2)
-	return hash1
+	return hash
